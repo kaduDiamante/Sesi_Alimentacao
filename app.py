b01c2886dcfa2740838b619911999cede97a3e6c
carlos eduardo diamante santos <kadudiamante2017@gmail.com>
2024-10-17 10:29:08 -0300

    link flask

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def pagina_inicial():
+    return render_template('pag_login.html')
+
+@app.route('/Dashboard')
+def Dashboard():
     return render_template('Dashboard.html')
+
+@app.route('/Dashboard_Aluno')
+def Dashboard_Aluno():
+    return render_template('Dashboard Aluno.html')
 
 @app.route('/desperdicio', methods=['POST', 'GET'])
 def desperdicio():
@@ -21,7 +29,7 @@ def desperdicio():
     else: 
         return render_template('desperdicio.html')
 
-@app.route('/cardapio', methods=['POST', 'GET'])
+@app.route('/set_cardapio', methods=['POST', 'GET'])
 def cardapio():
     if request.method == 'POST':
         informacao = request.get_json()
@@ -29,6 +37,34 @@ def cardapio():
         pyBanco.insert_img(imgCode)
     else:
         return render_template('Set_Cardapio.html')
+
+@app.route('/Perfil_nutricionista')
+def Perfil_nutricionista():
+    return render_template('Perfil_Nutricionista.html')
+
+@app.route('/Perfil_Aluno')
+def Perfil_Aluno():
+    return render_template('Perfil_Aluno.html')
+
+@app.route('/Refeicoes_alunos')
+def Refeicoes_alunos():
+    return render_template('Refeicoes alunos.html')
+
+@app.route('/historico_de_desperdicio_alunos')
+def historico_de_desperdicio_alunos():
+    return render_template('historico de desperdicio alunos.html')
+
+@app.route('/Tables')
+def Tables():
+    return render_template('Tables.html')
+
+@app.route('/Salas')
+def Salas():
+    return render_template('Salas.html')
+
+@app.route('/Set_agenda')
+def Set_agenda():
+    return render_template('Set_agenda.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
